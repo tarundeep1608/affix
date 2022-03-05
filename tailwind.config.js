baa7646fe3522b1fdb2195cf63f083bd21f8335b
Tarundeep Singh <72191456+tarundeep1608@users.noreply.github.com>
2022-03-05 11:58:17 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+// backgroundImage: {
+//   slide1: "url('/assets/images/slide-01.png')",
+//   slide2: "url('/assets/images/slide-02.png')",
+// }
